526956c98fdf108dc613c7df7f5bb02b263746af
Ketan Patil <Ketanp132134@gmail.com>
2023-11-23 21:20:01 +0530

    change in code to scrap more data

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ driver = webdriver.Edge(service = s)
 driver.get('https://www.smartprix.com/laptops')     # smartprix site url
 time.sleep(1)
 
-for i in range(0,10):      # Fetching data for only 10 pages  i.e. 200 items info
+for i in range(0,100):      # Fetching data for only 10 pages  i.e. 100 items info
     time.sleep(1)
     driver.find_element(by=By.XPATH,value='//*[@id="app"]/main/div[1]/div[3]/div[3]').click()
 html = driver.page_source
